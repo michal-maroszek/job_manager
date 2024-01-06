e002a27e4d9c10851cb778548bfba188fb502e0c
michal-maroszek <micmaroszek@gmail.com>
2024-01-06 16:31:38 +0100

    Basic handling of address geolocation errors

--- website/views.py
+++ website/views.py
@@ -20,11 +20,15 @@ def get_coords(address):
     if Job.location:
         geolocator = Nominatim(user_agent="job_manager")
         location = geolocator.geocode(address)
-        lat = location.latitude
-        lon = location.longitude
-        return lat, lon
-    else:
-        return None
+        if location == int:
+            try:
+                lat = location.latitude
+                lon = location.longitude
+                return lat, lon
+            except (AttributeError, TypeError, ValueError):
+                return (None, None)
+        else:
+            return (None, None)
 
 
 @views.route("/")
@@ -115,6 +119,7 @@ def delete_client(id):
 # ----------------------------------------------------------------
 
 
+# view for showing a list of all jobs and a form for adding new ones
 @views.route("/jobs_list", methods=["GET", "POST"])
 @login_required  # doesn't allow this function unless a user is logged in
 def jobs_list():
@@ -159,6 +164,7 @@ def jobs_list():
     return render_template("jobs_list.html", user=current_user)
 
 
+# view for editing job data
 @views.route("/job<int:id>", methods=["GET", "POST"])
 @login_required  # doesn't allow this function unless a user is logged in
 def current_job(id):
@@ -239,6 +245,12 @@ def filter_jobs(filter):
     """
 
 
+# ----------------------------------------------------------------
+# Jobs map
+# ----------------------------------------------------------------
+
+
+# view for rendering a map with all jobs markers shown
 @views.route("/jobs_map", methods=["GET", "POST"])
 @login_required  # doesn't allow this function unless a user is logged in
 def jobs_map():
@@ -248,26 +260,36 @@ def jobs_map():
 
     if current_user.jobs:
         for job in current_user.jobs:
-            coords = (job.lat, job.lon)
-            lat = job.lat
-            lon = job.lon
-            all_jobs_coord.append(coords)
-            all_jobs_lat.append(lat)
-            all_jobs_lon.append(lon)
+            # in case a job was given wrong adress and doesn't have valid coords
+            if job.lat or job.lon == int:
+                coords = (job.lat, job.lon)
+                lat = job.lat
+                lon = job.lon
+                all_jobs_coord.append(coords)
+                all_jobs_lat.append(lat)
+                all_jobs_lon.append(lon)
+            else:
+                continue
 
+        # calculating average coords to center the map based on all jobs markers
         avg_lat = sum(all_jobs_lat) / len(all_jobs_lat)
         avg_lon = sum(all_jobs_lon) / len(all_jobs_lon)
         average_coords = (avg_lat, avg_lon)
 
         map = folium.Map(location=average_coords, zoom_start=9, control_scale=True)
 
+        # creating markes for each job based on their coords
         for job_marker in current_user.jobs:
-            folium.Marker(
-                location=[job_marker.lat, job_marker.lon],
-                tooltip=job_marker.name,
-                popup=f"{job_marker.name}\n{job_marker.job_type}\n{job_marker.status}\n{job_marker.end_time}\n{job_marker.info}",
-                icon=folium.Icon(color="red"),
-            ).add_to(map)
+            # in case a job was given wrong adress and doesn't have valid coords
+            if job_marker.lat or job_marker.lon == int:
+                folium.Marker(
+                    location=[job_marker.lat, job_marker.lon],
+                    tooltip=job_marker.name,
+                    popup=f"{job_marker.name}\n{job_marker.job_type}\n{job_marker.status}\n{job_marker.end_time}\n{job_marker.info}",
+                    icon=folium.Icon(color="red"),
+                ).add_to(map)
+            else:
+                continue
     else:
         map = folium.Map(
             location=(51.9189046, 19.1343786), zoom_start=10, control_scale=True
@@ -276,23 +298,40 @@ def jobs_map():
     return render_template("jobs_map.html", user=current_user, map=map._repr_html_())
 
 
+# view for showing specific job marker on a map
 @views.route("/jobs_map<int:id>", methods=["GET", "POST"])
 @login_required  # doesn't allow this function unless a user is logged in
 def current_job_map(id):
     current_user_jobs = Job.query.filter_by(user=current_user.id)
     current_job = current_user_jobs.filter_by(id=id).first()
 
-    coords = (current_job.lat, current_job.lon)
-    lat = current_job.lat
-    lon = current_job.lon
+    if current_job.lat or current_job.lon == int:
+        coords = (current_job.lat, current_job.lon)
+        lat = current_job.lat
+        lon = current_job.lon
 
-    map = folium.Map(location=coords, zoom_start=15, control_scale=True)
+        map = folium.Map(location=coords, zoom_start=15, control_scale=True)
 
-    folium.Marker(
-        location=[lat, lon],
-        tooltip=current_job.name,
-        popup=f"{current_job.name}\n{current_job.job_type}\n{current_job.status}\n{current_job.end_time}\n{current_job.info}",
-        icon=folium.Icon(color="red"),
-    ).add_to(map)
+        folium.Marker(
+            location=[lat, lon],
+            tooltip=current_job.name,
+            popup=f"{current_job.name}\n{current_job.job_type}\n{current_job.status}\n{current_job.end_time}\n{current_job.info}",
+            icon=folium.Icon(color="red"),
+        ).add_to(map)
 
-    return render_template("jobs_map.html", user=current_user, map=map._repr_html_())
+        return render_template(
+            "jobs_map.html", user=current_user, map=map._repr_html_()
+        )
+
+    else:
+        flash(
+            "Selected job doesn't have a valid coordinates. Check job adress.",
+            category="job_error",
+        )
+        map = folium.Map(
+            location=(51.9189046, 19.1343786), zoom_start=10, control_scale=True
+        )
+
+        return render_template(
+            "jobs_map.html", user=current_user, map=map._repr_html_()
+        )
